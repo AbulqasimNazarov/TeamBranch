4b4917bf998d58723d243e982a76a3fffd93a6b4
Abulqasim <abulqasim.nazarov@gmail.com>
2023-11-20 19:33:29 +0400

    Print

--- TeamsGitBranchApp/TeamsGitBranchApp/Program.cs
+++ TeamsGitBranchApp/TeamsGitBranchApp/Program.cs
@@ -3,3 +3,7 @@ using TeamsGitBranchApp.FrontEndAnn;
 
 FrontClass frontObject = new FrontClass();
 BackClass backObject = new BackClass();
+
+
+System.Console.WriteLine(frontObject.ToString());
+System.Console.WriteLine(backObject.ToString());
