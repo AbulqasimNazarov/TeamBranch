045b12fcec84c0fab41492a4b77b732fa6d48524
Abulqasim <abulqasim.nazarov@gmail.com>
2023-11-20 19:01:26 +0400

    Developer

---
+++ TeamsGitBranchApp/TeamsGitBranchApp/FrontEndAnn/FrontClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamsGitBranchApp.FrontEndAnn
+{
+    public class FrontClass
+    {
+        
+    }
+}
